1767a082380600124a46977bbe0aacd7089f23cb
Salih <salih@hanifeoglu.com>
2018-06-17 17:59:05 +0300

    Teslim öncesi

--- table.php
+++ table.php
@@ -1,13 +1,16 @@
 <link rel="stylesheet" href="css/custom.css">
 <?php
-
+//get url in files name
 $url = $_GET["FilesName"];
 
+//read  it upload folder in grt url in files name
 $file = file_get_contents('uploads/'.$url);
 
+//explode it pear with space after
 $lines = explode("\n", $file);
 //unset($lines[0]);
 
+//start table here
 echo '<table>';
 foreach ($lines as $key => &$line) {
     echo '<tr>';
@@ -19,9 +22,9 @@ foreach ($lines as $key => &$line) {
 
             echo '<th>' . $item. '</th>';
     }
-    echo '</tr>';
+    echo '</tr>'; // end td
 }
-echo '</table>';
+echo '</table>'; // end table
 
 
 
